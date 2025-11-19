729d32968d30e60ccedab690119df3675460029e
Prathmesh Rodekar <prathmeshrodekar6@gmail.com>
2025-11-20 03:02:38 +0530

    Frist commit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,13 +1,14 @@
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/Config/inngest";
-
+import {
+  inngest,
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+} from "@/Config/inngest";
+import { server } from "typescript";
 
 // Create an API that serves zero functions
-export const { GET, POST, PUT } = serve({
+export const { GET, POST, PUT } = server({
   client: inngest,
-  functions: [
-   syncUserCreation,
-   syncUserUpdation,
-   syncUserDeletion
-  ],
+  functions: [syncUserCreation, syncUserUpdation, syncUserDeletion],
 });
